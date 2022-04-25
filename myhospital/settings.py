c01b0d5d93f16c4df6812879a82fd3f41813bf32
bradiepoa <bradiepoa7@gmail.com>
2022-04-25 23:59:48 +0300

    done

--- myhospital/settings.py
+++ myhospital/settings.py
@@ -124,11 +124,11 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'gettreat.User'
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
